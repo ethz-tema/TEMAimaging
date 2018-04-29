15d74e550af5319346ff01db9f92fbc5ad31285f
Peter Keresztes Schmidt <peterke@student.ethz.ch>
2018-04-29 22:01:06 +0200

    renderers: Move renderers to their own file

--- wxgui.py
+++ wxgui.py
@@ -7,6 +7,7 @@ from hardware.laser_compex import CompexLaserProtocol, OpMode
 from hardware.mcs_stage import MCSAxis
 from scans import MeasurementController
 from gui.preferences import PreferencesDialog
+from gui.renderers import SequenceEditorTextRenderer, SequenceEditorToggleRenderer
 
 DEBUG = False
 
@@ -209,85 +210,6 @@ class MeasurementViewModel(wx.dataview.PyDataViewModel):
         return True
 
 
-class MyToggleRenderer(wx.dataview.DataViewCustomRenderer):
-    def __init__(self, *args, **kwargs):
-        super().__init__('PyObject', wx.dataview.DATAVIEW_CELL_ACTIVATABLE, *args, **kwargs)
-        self.value = None
-
-    def SetValue(self, value):
-        self.value = value
-        return True
-
-    def GetValue(self):
-        return False
-
-    def GetSize(self):
-        return wx.RendererNative.Get().GetCheckBoxSize(self.GetView())
-
-    def Render(self, cell, dc, state):
-        if not self.value[0]:
-            return True
-
-        flags = 0
-        if self.value[1]:
-            flags |= wx.CONTROL_CHECKED
-
-        wx.RendererNative.Get().DrawCheckBox(self.GetOwner().GetOwner(), dc, cell, flags)
-        return True
-
-    def ActivateCell(self, cell, model, item, col, mouseEvent):
-        if mouseEvent:
-            if not wx.Rect(self.GetSize()).Contains(mouseEvent.GetPosition()):
-                return False
-
-            model.ChangeValue(not self.value[1], item, col)
-            return True
-
-
-class MyTextRenderer(wx.dataview.DataViewCustomRenderer):
-    def __init__(self, *args, **kwargs):
-        super().__init__('PyObject', wx.dataview.DATAVIEW_CELL_EDITABLE, *args, **kwargs)
-        self.value = ""
-
-    def SetValue(self, value):
-        self.value = value
-        return True
-
-    def GetValue(self):
-        return False
-
-    def HasEditorCtrl(self):
-        return self.value[0] and self.value[1]
-
-    def CreateEditorCtrl(self, parent, labelRect, value):
-        ctrl = wx.TextCtrl(parent,
-                           value=value[2],
-                           pos=labelRect.Position,
-                           size=labelRect.Size)
-
-        # select the text and put the caret at the end
-        ctrl.SetInsertionPointEnd()
-        ctrl.SelectAll()
-
-        return ctrl
-
-    def GetValueFromEditorCtrl(self, editor):
-        return editor.GetValue()
-
-    def Render(self, cell, dc, state):
-        if not self.value[0]:
-            return True
-
-        self.RenderText(self.value[2], 0, cell, dc, state)
-        return True
-
-    def GetSize(self):
-        if self.value[0] and self.value[2]:
-            return self.GetTextExtent(self.value[2])
-
-        return wx.Size(wx.dataview.DVC_DEFAULT_RENDERER_SIZE, wx.dataview.DVC_DEFAULT_RENDERER_SIZE)
-
-
 class MeasurementPanel(wx.Panel):
     def __init__(self, parent, *args, **kwargs):
         super(MeasurementPanel, self).__init__(parent, wx.ID_ANY, *args, **kwargs)
@@ -304,31 +226,31 @@ class MeasurementPanel(wx.Panel):
         c0 = self.dvc.AppendTextColumn('Step', 0)
         c0.SetMinWidth(70)
 
-        c1 = wx.dataview.DataViewColumn('Type', MyTextRenderer(), 1, width=100)
+        c1 = wx.dataview.DataViewColumn('Type', SequenceEditorTextRenderer(), 1, width=100)
         c1.SetMinWidth(100)
         self.dvc.AppendColumn(c1)
 
-        c2 = wx.dataview.DataViewColumn('', MyTextRenderer(), 2, width=100)
+        c2 = wx.dataview.DataViewColumn('', SequenceEditorTextRenderer(), 2, width=100)
         c2.SetMinWidth(50)
         self.dvc.AppendColumn(c2)
 
-        c3 = wx.dataview.DataViewColumn('', MyTextRenderer(), 3)
+        c3 = wx.dataview.DataViewColumn('', SequenceEditorTextRenderer(), 3)
         c3.SetMinWidth(50)
         self.dvc.AppendColumn(c3)
 
-        c4 = wx.dataview.DataViewColumn('Spot Size', MyTextRenderer(), 4)
+        c4 = wx.dataview.DataViewColumn('Spot Size', SequenceEditorTextRenderer(), 4)
         c4.SetMinWidth(50)
         self.dvc.AppendColumn(c4)
 
-        c5 = wx.dataview.DataViewColumn('Frequency', MyTextRenderer(), 5)
+        c5 = wx.dataview.DataViewColumn('Frequency', SequenceEditorTextRenderer(), 5)
         c5.SetMinWidth(50)
         self.dvc.AppendColumn(c5)
 
-        c6 = wx.dataview.DataViewColumn('Shots per Spot', MyTextRenderer(), 6, width=100)
+        c6 = wx.dataview.DataViewColumn('Shots per Spot', SequenceEditorTextRenderer(), 6, width=100)
         c6.SetMinWidth(50)
         self.dvc.AppendColumn(c6)
 
-        c7 = wx.dataview.DataViewColumn("Cleaning shot", MyToggleRenderer(), 7)
+        c7 = wx.dataview.DataViewColumn("Cleaning shot", SequenceEditorToggleRenderer(), 7)
         c7.SetMinWidth(50)
         self.dvc.AppendColumn(c7)
 
