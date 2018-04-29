a0915370ded93d3a5b4f10b4819f182d645c0009
Peter Keresztes Schmidt <peterke@student.ethz.ch>
2018-04-29 22:07:38 +0200

    dialogs: Move AddScanDialog to separate file
    Simple dialogs should be put into this file.

--- wxgui.py
+++ wxgui.py
@@ -3,11 +3,12 @@ import wx.dataview
 import wx.lib.pubsub.pub as pub
 
 from connection_mgr import conn_mgr, ConnectionManagerDialog
+from gui.dialogs import AddScanDialog
+from gui.preferences import PreferencesDialog
+from gui.renderers import SequenceEditorTextRenderer, SequenceEditorToggleRenderer
 from hardware.laser_compex import CompexLaserProtocol, OpMode
 from hardware.mcs_stage import MCSAxis
 from scans import MeasurementController
-from gui.preferences import PreferencesDialog
-from gui.renderers import SequenceEditorTextRenderer, SequenceEditorToggleRenderer
 
 DEBUG = False
 
@@ -286,45 +287,6 @@ class MeasurementPanel(wx.Panel):
             self.model.ItemAdded(wx.dataview.NullDataViewItem, step_item)
             for param in step.params.values():
                 self.model.ItemAdded(step_item, self.model.ObjectToItem(param))
-
-
-class AddScanDialog(wx.Dialog):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.scan_choices = {scan.__name__: scan
-                             for scan in MeasurementController.scan_types}
-        self.choice_scan_type = wx.Choice(self, wx.ID_ANY, choices=list(self.scan_choices))
-        self.btn_add = wx.Button(self, wx.ID_ADD, "")
-        self.btn_cancel = wx.Button(self, wx.ID_CANCEL, "")
-
-        self.init_ui()
-
-    def init_ui(self):
-        self.SetTitle("Add Scan")
-        self.choice_scan_type.SetSelection(0)
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-
-        choice_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        lbl_scan_type = wx.StaticText(self, wx.ID_ANY, "Scan Type:")
-        choice_sizer.Add(lbl_scan_type, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        choice_sizer.Add(self.choice_scan_type, 0, wx.ALIGN_CENTER | wx.RIGHT, 5)
-        sizer.Add(choice_sizer, 1, wx.EXPAND, 0)
-
-        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        btn_sizer.Add(self.btn_add, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
-        btn_sizer.Add(self.btn_cancel, 0, wx.ALIGN_CENTER_VERTICAL | wx.BOTTOM | wx.RIGHT | wx.TOP, 5)
-        sizer.Add(btn_sizer, 1, wx.EXPAND, 0)
-
-        self.Bind(wx.EVT_BUTTON, self.on_click_add, self.btn_add)
-
-        self.SetSizer(sizer)
-        sizer.Fit(self)
-        self.Layout()
-
-    def on_click_add(self, e):
-        self.EndModal(wx.ID_ADD)
 
 
 class LaserPanel(wx.Panel):
