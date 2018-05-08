ba1349f488ad2f538375428783de3d43c5272157
Peter Keresztes Schmidt <peterke@student.ethz.ch>
2018-05-08 03:12:12 +0200

    ContinuousLineScan: Implement the continuous line scan

---
+++ scans/cont_line.py
@@ -0,0 +1,103 @@
+import math
+
+from core.conn_mgr import conn_mgr
+from core.scanner_registry import ScannerMeta
+from hardware.mcs_stage import MCSAxis
+
+
+class ContinuousLineScan(metaclass=ScannerMeta):
+    parameter_map = {'spot_count': ('Spot Count', 1, None),
+                     'direction': ('Direction', 0.0, None),
+                     'x_start': ('X (Start)', 0.0, 1e-6),
+                     'y_start': ('Y (Start)', 0.0, 1e-6),
+                     'z_start': ('Z (Start)', 0.0, 1e-6),
+                     'z_end': ('Z (End)', 0.0, 1e-6)}
+
+    display_name = "Cont. Line Scan"
+
+    def __init__(self, spot_size, shot_count=1, frequency=1, cleaning=False, spot_count=1, direction=0, x_start=None,
+                 y_start=None, z_start=None, dz=None):
+        self.spot_size = spot_size
+        self.spot_count = spot_count
+        self.shot_count = shot_count
+        self.direction = math.radians(direction)
+        self.x_start = x_start
+        self.y_start = y_start
+        self.z_start = z_start
+        self.frequency = frequency
+
+        self._vx_orig = conn_mgr.stage.get_speed(MCSAxis.X)
+        self._vy_orig = conn_mgr.stage.get_speed(MCSAxis.Y)
+        self._vz_orig = conn_mgr.stage.get_speed(MCSAxis.Z)
+
+        self._curr_step = 0
+        self._vx = math.sin(self.direction) * self.spot_size * self.frequency / shot_count
+        self._vy = math.cos(self.direction) * self.spot_size * self.frequency / shot_count
+
+        v = math.sqrt(math.pow(self._vx, 2) + math.pow(self._vy, 2))
+
+        self._dx = math.sin(self.direction) * self.spot_size * spot_count
+        self._dy = math.cos(self.direction) * self.spot_size * spot_count
+        self._dz = dz
+
+        time = spot_size * spot_count / v
+        self._vz = dz / time
+
+    @classmethod
+    def from_params(cls, spot_size, shot_count, frequency, cleaning, params):
+        spot_count = params['spot_count'].value
+
+        if spot_count > 1 and params['z_start'].value and params['z_end']:
+            dz = params['z_end'].value - params['z_start'].value
+        else:
+            dz = 0
+
+        return cls(spot_size, shot_count, frequency, cleaning, spot_count, params['direction'].value,
+                   params['x_start'].value, params['y_start'].value, params['z_start'].value, dz)
+
+    def init_scan(self):
+        if self.x_start:
+            conn_mgr.stage.move(MCSAxis.X, self.x_start, wait=False)
+        if self.y_start:
+            conn_mgr.stage.move(MCSAxis.Y, self.y_start, wait=False)
+        if self.z_start:
+            conn_mgr.stage.move(MCSAxis.Z, self.z_start, wait=False)
+        conn_mgr.trigger.set_count(self.spot_count * self.shot_count)
+        conn_mgr.trigger.set_freq(self.frequency)
+
+        conn_mgr.stage.wait_until_status()
+
+        if self._vx != 0:
+            conn_mgr.stage.set_speed(self._vx, MCSAxis.X)
+        if self._vy != 0:
+            conn_mgr.stage.set_speed(self._vy, MCSAxis.Y)
+        if self._vz != 0:
+            conn_mgr.stage.set_speed(self._vz, MCSAxis.Z)
+
+    def next_move(self):
+        if self._curr_step >= self.spot_count:
+            return False
+
+        conn_mgr.trigger.go()
+
+        if self._curr_step + 1 >= self.spot_count:  # skip move after last shot
+            return False
+
+        if self._dx != 0:
+            conn_mgr.stage.move(MCSAxis.X, self._dx, relative=True, wait=False)
+        if self._dy != 0:
+            conn_mgr.stage.move(MCSAxis.Y, self._dy, relative=True, wait=False)
+        if self._dz != 0:
+            conn_mgr.stage.move(MCSAxis.Z, self._dz, relative=True, wait=False)
+
+        self._curr_step += 1
+        conn_mgr.stage.wait_until_status()
+
+        # conn_mgr.stage.set_speed(self._vx_orig, MCSAxis.X)
+        # conn_mgr.stage.set_speed(self._vy_orig, MCSAxis.Y)
+        # conn_mgr.stage.set_speed(self._vz_orig, MCSAxis.Z)
+        conn_mgr.stage.set_speed(0)
+        return False
+
+    def next_shot(self):
+        pass
