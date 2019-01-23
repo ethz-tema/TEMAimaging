0b8aa3d5a8f986fe82f1209df3d4fc6913d6864d
Peter Keresztes Schmidt <peterke@student.ethz.ch>
2019-01-23 16:42:15 +0100

    Activate wx InspectionMixin in debug mode

--- wxgui.py
+++ wxgui.py
@@ -1,12 +1,18 @@
 import logging
 
 import wx.dataview
+import wx.lib.mixins.inspection as wit
+
+DEBUG = False
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class GeolasPyApp(wx.App):
+class GeolasPyApp(wx.App, wit.InspectionMixin):
     def OnInit(self):
+        if DEBUG:
+            self.Init()
+
         from gui.main_frame import MainFrame
         frm = MainFrame(None, title="geolasPy")
 
